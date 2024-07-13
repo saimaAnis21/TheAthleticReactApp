2d328a2a438c21dd609e9b5634694d333566582d
Saima <saima_rehman21@yahoo.com>
2024-07-14 00:06:23 +1000

    Create a page to list all articles relevant to the selected teams/leagues in reverse chronological order

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -99,6 +99,11 @@ export default function Articles({followedTeams:teamIds, followedLeagues:leagueI
   forEach(data?.articles, (item) => {
     if (includes(allTeamids, item.team.id) || includes(allLeagueids, item.league.id)) { articles.push(item); }
   });
+  articles.sort((a, b) => {
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+   return dateB - dateA ;
+  })
   if (loading) <div>Loading....</div>
   
     return (
